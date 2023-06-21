15550d0703973e384edbb6b5c08a7e401a362237
mohamedhefny617 <mohamedhefny617@gmail.com>
2023-06-20 19:05:21 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h" 
+
+/**
+ * main - Entry point
+ *
+ * Description: 'charC program'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char charC[] = "_putchar\n";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(charC[i]);
+}
+
+return (0);
+}
+
